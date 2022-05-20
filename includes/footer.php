87bdb2ce8a3b80738ffab42387293efd184cca82
JulianW051 <1194356@student.roc-nijmegen.nl>
2022-05-20 09:11:35 +0200

    Front end begin

---
+++ includes/footer.php
@@ -0,0 +1,13 @@
+<footer>
+    <div class="container">
+        <div class="left">
+
+        </div>
+        <div class="middle">
+            <p>Copyright &copy; 2022 (website naam). All Rights Reserved</p>
+        </div>
+        <div class="right">
+
+        </div>
+    </div>
+</footer>
