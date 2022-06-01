8e3c3adced28d39175acd54700c46d02c3860380
TheunissenSil <theunissensil@gmail.com>
2022-06-01 13:49:01 +0200

    slideshow

--- includes/header.php
+++ includes/header.php
@@ -9,30 +9,69 @@
             echo "<link rel='stylesheet' href='css/styles.css'>";
         } else if ($page === "overOns") {
             echo "<link rel='stylesheet' href='../css/styles.css'>";
-        } else if ($page === "contact") {
+        } else if ($page === "contact") {   
             echo "<link rel='stylesheet' href='../css/styles.css'>";
         }
     ?>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
     <title>CRUD</title>
 </head>
-<body>
-<header>
-    <div class="header-buttons">
-        <?php
-            if ($page === "home") {
-                echo "<a href='index.php'><button>Home</button></a>";
-                echo "<a href='pages/over-ons.php'><button>over ons</button></a>";
-                echo "<a href='pages/contact.php'><button>contact</button></a>";
-            } else if ($page === "overOns") {
-                echo "<a href='../index.php'><button>Home</button></a>";
-                echo "<a href='over-ons.php'><button>over ons</button></a>";
-                echo "<a href='contact.php'><button>contact</button></a>";
-            } else if ($page === "contact") {
-                echo "<a href='../index.php'><button>Home</button></a>";
-                echo "<a href='over-ons.php'><button>over ons</button></a>";
-                echo "<a href='contact.php'><button>contact</button></a>";
-            }
-        ?>
-    </div>
-</header>
+<body>  
+    <header>
+        <nav>
+            <div class="header-buttons">
+                <?php
+                    // Home
+                    if ($page === "home") {
+                        echo "<a href='index.php'><button>Home</button></a>";
+                        echo "<a href='pages/over-ons.php'><button>over ons</button></a>";
+                        echo "<a href='pages/Boeken.php'><button>Boeken</button></a>";
+                        echo "<a href='pages/contact.php'><button>contact</button></a>";
+                        echo "<a href='pages/mijn-account.php'><button>Mijn Account</button></a>";
+
+                    // Over ons
+                    } else if ($page === "overOns") {
+                        echo "<a href='../index.php'><button>Home</button></a>";
+                        echo "<a href='over-ons.php'><button>over ons</button></a>";
+                        echo "<a href='pages/Boeken.php'><button>Boeken</button></a>";
+                        echo "<a href='pages/contact.php'><button>contact</button></a>";
+                        echo "<a href='pages/mijn-account.php'><button>Mijn Account</button></a>";
+
+                    // Boeken 
+                    } else if ($page === "Boeken") {
+                        echo "<a href='../index.php'><button>Home</button></a>";
+                        echo "<a href='over-ons.php'><button>over ons</button></a>";
+                        echo "<a href='boeken.php'><button>Boeken</button></a>";
+                        echo "<a href='pages/contact.php'><button>contact</button></a>";
+                        echo "<a href='pages/mijn-account.php'><button>Mijn Account</button></a>";
+
+                    // Contact
+                    } else if ($page === "contact") {
+                        echo "<a href='../index.php'><button>Home</button></a>";
+                        echo "<a href='over-ons.php'><button>over ons</button></a>";
+                        echo "<a href='pages/boeken.php'><button>Boeken</button></a>";
+                        echo "<a href='contact.php'><button>contact</button></a>";
+                        echo "<a href='pages/mijn-account.php'><button>Mijn Account</button></a>";
+
+                    // Mijn Account 
+                    } else if ($page === "mijnAccount") {
+                        echo "<a href='../index.php'><button>Home</button></a>";
+                        echo "<a href='over-ons.php'><button>over ons</button></a>";
+                        echo "<a href='pages/boeken.php'><button>Boeken</button></a>";
+                        echo "<a href='pages/contact.php'><button>contact</button></a>";
+                        echo "<a href='pages/mijn-account.php'><button>Mijn Account</button></a>";
+                    }
+                ?>
+            </div>
+        </nav>
+        
+        <div class="slideshow-container">
+
+            <div class="mySlides slide1"></div>
+
+            <div class="mySlides slide2"></div>
+
+            <div class="mySlides slide3"></div>
+        </div>
+
+    </header>
