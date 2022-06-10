136c8100e420004ad47ebb8e4912834d21e02f25
JulianW051 <1194356@student.roc-nijmegen.nl>
2022-06-10 10:56:00 +0200

    blok 1

--- index.php
+++ index.php
@@ -3,7 +3,14 @@ $page = "home";
 include("includes/header.php")
 ?>
 <main>
+    <div class="home">
+        <div class="blok-1">
+            <a href="pages/boeken.php"><h2>Boeken ></h2></a>
+        </div>
+        <div class="blok-2">
 
+        </div>
+    </div>
 </main> 
 <?php
 include("includes/footer.php")
